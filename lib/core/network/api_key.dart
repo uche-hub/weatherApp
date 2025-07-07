64789059059b815273ec719a84b2b6e69c6c971d
uche-hub <ucj.theuche@gmail.com>
2025-07-07 19:28:39 +0100

    Add .gitignore and API key

---
+++ lib/core/network/api_key.dart
@@ -0,0 +1,1 @@
+const String apiKey = 'e0ce9aad1e739325a70f3f229a026e81';
--- lib/features/weather/data/repos/weather_repo.dart
+++ lib/features/weather/data/repos/weather_repo.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:weather_app/core/network/api_key.dart';
 import 'package:weather_app/features/weather/data/models/city_weather.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
@@ -79,7 +80,7 @@ class WeatherRepository {
     if (!_box.values.map((cw) => cw.cityName).contains(cityName)) {
       final response = await http.get(
         Uri.parse(
-          'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=e0ce9aad1e739325a70f3f229a026e81&units=metric',
+          'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey&units=metric',
         ),
       );
 
@@ -188,7 +189,7 @@ class WeatherRepository {
       // Fetch weather data from OpenWeatherMap API using coordinates
       final response = await http.get(
         Uri.parse(
-          'https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=e0ce9aad1e739325a70f3f229a026e81&units=metric',
+          'https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=$apiKey&units=metric',
         ),
       );
 
